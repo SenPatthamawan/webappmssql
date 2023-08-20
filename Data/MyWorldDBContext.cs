77f2f49894f75337db3c2b71f9dad524ae09db95
senpatthamawan <sen.patthamawan@gmail.com>
2023-08-20 17:00:35 +0700

    initial project

---
+++ Data/MyWorldDBContext.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using webappmssql.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace webappmssql.Data
+{
+    public class MyWorldDBContext: DbContext
+    {
+        
+    }
+}
